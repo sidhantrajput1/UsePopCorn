b37d321b1ce11327a0ac8fa346fd3775c9c96d86
sidhantrajput1 <sidhant808310@gmail.com>
2025-01-11 12:47:09 +0530

    Star Rating

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode} from "react";
 import { createRoot } from "react-dom/client";
-import './index.css'
-import App from './App.jsx'
+// import './index.css'
+// import App from './App.jsx'
+import StarRating1 from "./StarRating1";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <StarRating1 maxRating={5} /> 
+    {/* <StarRating1 maxRating={10} /> */}
   </StrictMode>
 );
